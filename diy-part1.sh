de06dd7fd61b2738bd9655091086db67ccb0ed03
xiaoxue508 <xiaoxue508@gmail.com>
2026-01-04 15:24:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. 取消注释 feed sources (如果默认被注释的话)
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 2. 添加 Passwall 及其依赖源 (使用 xiaorouji 的源，适配性最好)
+# 注意：ImmortalWrt 自带了不少包，但为了 Passwall 最新版，建议手动添加
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+
+# 3. (可选) 添加其他常用包源，如 OpenClash 等，不需要可忽略
+# echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
